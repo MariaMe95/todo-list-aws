37ef810711bde9d376e368862dd89659dddaaed7
MARIA MERINO DE LA CUEVA <131667243+MariaMe95@users.noreply.github.com>
2023-06-03 19:08:02 +0200

    Update deploy.sh
    add name bucket s3

--- pipelines/common-steps/deploy.sh
+++ pipelines/common-steps/deploy.sh
@@ -3,3 +3,4 @@
 set -x
 du -hs * | sort -h
 sam deploy template.yaml --config-env ${ENVIRONMENT} --no-confirm-changeset --force-upload --no-fail-on-empty-changeset --no-progressbar
+sam --s3-bucket s3_bucket

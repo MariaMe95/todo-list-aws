5a3b30ae20e0bb81039d26062cb0d70f375a6a04
MARIA MERINO DE LA CUEVA <131667243+MariaMe95@users.noreply.github.com>
2023-06-04 16:40:28 +0200

    Update todoApiTest.py

--- test/integration/todoApiTest.py
+++ test/integration/todoApiTest.py
@@ -30,7 +30,7 @@ class TestApi(unittest.TestCase):
         }
         response = requests.post(url, data=json.dumps(data))
         json_response = response.json()
-        print('Response Add Todo: '+ str(json_response['body]))
+        print('Response Add Todo: '+ str(json_response['body']))
         jsonbody= json.loads(json_response['body'])
         ID_TODO = jsonbody['id']
         print ('ID todo:'+ID_TODO)
